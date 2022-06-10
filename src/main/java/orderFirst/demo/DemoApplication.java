69b8ce91e421216e3d5474391e563ddc553dbe76
natsuki221 <linnatsuki221@gmail.com>
2022-06-10 16:35:04 +0800

    natsuki221

--- src/main/java/orderFirst/demo/DemoApplication.java
+++ src/main/java/orderFirst/demo/DemoApplication.java
@@ -3,9 +3,10 @@ package orderFirst.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
